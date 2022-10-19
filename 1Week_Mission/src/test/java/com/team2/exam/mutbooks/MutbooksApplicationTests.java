c04e6f6b13bd1bdec101da17d3afc788a95e4fb9
SeungKeun Choi <77659341+ddackkeun@users.noreply.github.com>
2022-10-19 18:54:11 +0900

    chore: 프로젝트 세팅

---
+++ 1Week_Mission/src/test/java/com/team2/exam/mutbooks/MutbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.exam.mutbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
